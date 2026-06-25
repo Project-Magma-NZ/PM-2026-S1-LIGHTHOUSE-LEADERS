1af5c39aa0ba4428b84bb281c2c6205d320c6012
rebeccafitzpatr <rebecca.fitzpatr@gmail.com>
2026-06-25 17:02:38 +1200

    change package import

--- api/init_db.py
+++ api/init_db.py
@@ -1,5 +1,5 @@
-from db import Base, engine
-from models import User, School
+from .db import Base, engine
+from .models import User, School
 
 if __name__ == "__main__":
     Base.metadata.create_all(bind=engine)
--- api/security.py
+++ api/security.py
@@ -1,7 +1,6 @@
 import os
 from datetime import datetime, timedelta, timezone
 from typing import Any
-
 from dotenv import load_dotenv
 from jose import jwt
 from passlib.context import CryptContext
